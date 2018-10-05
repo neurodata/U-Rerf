e0d6b8e75108c8b40be413c6e682db8de4841de7
James Browne <jbrowne6@jhu.edu>
2018-10-05 09:30:52 -0400

    modified ex10 to show closer clusters and modified ex13 to be executable on something other than awesomer

--- examples/ex10Clustering.R
+++ examples/ex10Clustering.R
@@ -7,7 +7,7 @@ library(MASS)
 # number of trees for forest
 numtrees <- 100
 # number of samples in dataset
-sizeD <- 400
+sizeD <- 1000
 # the 'k' of k nearest neighbors
 depth = 8
 k = 3
@@ -18,15 +18,17 @@ X <- swissRoll(sizeD/2, size =1, dim3=T)
 X <- as.matrix(X)[,2:4]
 #X <- X[order(X[,3], X[,1], X[,2]),]
 
-X[1:(sizeD/2),] <- X[1:(sizeD/2),]-3
-X[(sizeD/2+1):sizeD,] <- X[(sizeD/2+1):sizeD,]+3
+#X[1:(sizeD/2),] <- X[1:(sizeD/2),]-3
+X[(sizeD/2+1):sizeD,1] <- X[(sizeD/2+1):sizeD,1]-1.5
+
+actualClusters <- c(rep(1,sizeD/2), rep(2,sizeD/2))
 
 #create output
 png(file="results/ex10_Two3dswissRoll.png")
 with(data.frame(X), {
    scatterplot3d(x2, x3, x1,        # x y and z axis
-                 pch=19, # filled blue circles
-                 main="3-D Swiss Roll",
+                 color=actualClusters, pch=19, # filled blue circles
+                 main="Split 3-D Swiss Roll Actual Two Clusters",
                  xlab="X",
                  ylab="Y",
                  zlab="Z")
@@ -42,7 +44,7 @@ png(file="results/ex10_Two3dswissRollClustered.png")
 with(data.frame(X), {
    scatterplot3d(x2, x3, x1,        # x y and z axis
                  color=clusters, pch=19, # filled blue circles
-                 main="3-D Swiss Roll",
+                 main="Split 3-D Swiss Roll Calculated Two Clusters",
                  xlab="X",
                  ylab="Y",
                  zlab="Z")
